cbf75017822a7eaf16a41cb183a91c5e66e896e5
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-12-07 15:25:24 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,33 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name
+    self.genre.name
+  end
+
+  def genre_name=(genre_name)
+    self.genre = Genre.find_or_create_by(name: genre_name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by(name: artist_name)
+  end
+
+  def note_contents
+    self.note.contents
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      if !note.contents.empty?
+        self.notes << Note.find_or_create_by(contents: note_name)
+      end
+    end
+  end
 end
 
